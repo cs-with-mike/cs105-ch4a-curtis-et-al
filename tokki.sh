aefb990aaacfcd2c490a79eedce906b4b80735b7
Curtis Barnhart <cbarnhart@westmont.edu>
2023-09-25 17:19:46 -0700

    added my name to tokki.sh and started adding some basic set up

--- tokki.sh
+++ tokki.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 # Authors:
-#  - Boaty McBoatface, bmcboatface@westmont.edu
+#  - Curtis Barnhart, cbarnhart@westmont.edu
 #  - Thomas McTankengine, tmctankengine@westmont.edu
 #  - Ovenly McEasybake, omceasybake@westmont.edu
 
 # Run any commands necessary to set up your language's runtime environment here.
 # If the runtime is expected to be present on Ubuntu by default, then do nothing.
 echo "Setting up runtime ..."
+sudo apt install gcc
+sudo apt install cmake
 
 # I'm checking Python interpreter's version here just as a placeholder.
 python3 --version # TODO: YOUR RUNTIME SETUP HERE.
@@ -14,6 +16,8 @@ python3 --version # TODO: YOUR RUNTIME SETUP HERE.
 # Run your Tokki lexer, passing in the first command line argument directly to the lexer.
 # Any output to STDOUT should be directed to a text output file titled "out.txt."
 echo "Running Tokki ..."
+./tokki $1 >> out.txt
 
 # As an example, I have provided how I would run my tokki.pyc.
-python3 tokki.pyc $1 > out.txt # TODO: REPLACE THIS WITH YOUR OWN COMMAND.
+# python3 tokki.pyc $1 > out.txt # TODO: REPLACE THIS WITH YOUR OWN COMMAND.
+
