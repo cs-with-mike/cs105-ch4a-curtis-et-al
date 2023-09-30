f7bb583a953342a3624b64256414a42d7d6b0928
Curtis Barnhart <cbarnhart@westmont.edu>
2023-09-29 20:57:15 -0700

    db

---
+++ BUILD/CMakeFiles/tokki.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tokki.dir/main.cpp.o"
+  "CMakeFiles/tokki.dir/main.cpp.o.d"
+  "tokki"
+  "tokki.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tokki.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- tokki.sh
+++ tokki.sh
@@ -11,22 +11,28 @@ echo "Setting up runtime ..."
 # I'm checking Python interpreter's version here just as a placeholder.
 python3 --version # TODO: YOUR RUNTIME SETUP HERE.
 
-echo "Building Tokki ..."
+echo "BEGIN TOKKI BUILD--------------------------------------------------------------------------------"
 cmake -S . -O RELEASE_BUILD
 cd RELEASE_BUILD
 make
 cd ..
+echo "END TOKKI BUILD----------------------------------------------------------------------------------"
 
-
-echo "BEGIN PRINTING FILE"
+echo "BEGIN PRINTING FILE------------------------------------------------------------------------------"
 cat $1
-echo "END PRINTING FILE"
+echo "END PRINTING FILE--------------------------------------------------------------------------------"
 
 
 # Run your Tokki lexer, passing in the first command line argument directly to the lexer.
 # Any output to STDOUT should be directed to a text output file titled "out.txt."
-echo "Running Tokki ..."
+echo "BEGIN RUN TOKKI----------------------------------------------------------------------------------"
 ./RELEASE_BUILD/tokki $1 > out.txt
+echo "END RUN TOKKI------------------------------------------------------------------------------------"
+
+echo "BEGIN PRINT RESULTS------------------------------------------------------------------------------"
+echo out.txt
+echo "END PRINT RESULTS--------------------------------------------------------------------------------"
+
 
 # As an example, I have provided how I would run my tokki.pyc.
 # python3 tokki.pyc $1 > out.txt # TODO: REPLACE THIS WITH YOUR OWN COMMAND.
