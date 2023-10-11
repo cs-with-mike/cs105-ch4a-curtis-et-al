e5016b6254271e4fd0bdfa30abbadf2f4f6db75d
Curtis Barnhart <cbarnhart@westmont.edu>
2023-10-10 22:46:16 -0700

    d

--- tokki.sh
+++ tokki.sh
@@ -16,9 +16,17 @@ cd RELEASE_BUILD
 make
 cd ..
 
+echo "------------------------------------------------------------------------------"
+cat $1
+echo "------------------------------------------------------------------------------"
+
 # Run your Tokki lexer, passing in the first command line argument directly to the lexer.
 # Any output to STDOUT should be directed to a text output file titled "out.txt."
 ./RELEASE_BUILD/tokki p $1 out.txt
+
+echo "------------------------------------------------------------------------------"
+cat out.txt
+echo "------------------------------------------------------------------------------"
 
 # As an example, I have provided how I would run my tokki.pyc.
 # python3 tokki.pyc $1 > out.txt # TODO: REPLACE THIS WITH YOUR OWN COMMAND.
