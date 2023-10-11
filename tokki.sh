cd4d2be885ab430ae3b3456c3c687719c31be0e7
Curtis Barnhart <cbarnhart@westmont.edu>
2023-10-10 22:44:41 -0700

    update shell script

--- tokki.sh
+++ tokki.sh
@@ -11,28 +11,14 @@ echo "Setting up runtime ..."
 # I'm checking Python interpreter's version here just as a placeholder.
 python3 --version # TODO: YOUR RUNTIME SETUP HERE.
 
-echo "BEGIN TOKKI BUILD--------------------------------------------------------------------------------"
 cmake -S . -O RELEASE_BUILD
 cd RELEASE_BUILD
 make
 cd ..
-echo "END TOKKI BUILD----------------------------------------------------------------------------------"
-
-echo "BEGIN PRINTING FILE------------------------------------------------------------------------------"
-cat $1
-echo "END PRINTING FILE--------------------------------------------------------------------------------"
-
 
 # Run your Tokki lexer, passing in the first command line argument directly to the lexer.
 # Any output to STDOUT should be directed to a text output file titled "out.txt."
-echo "BEGIN RUN TOKKI----------------------------------------------------------------------------------"
-./RELEASE_BUILD/tokki $1
-echo "END RUN TOKKI------------------------------------------------------------------------------------"
-
-echo "BEGIN PRINT RESULTS------------------------------------------------------------------------------"
-cat out.txt
-echo "END PRINT RESULTS--------------------------------------------------------------------------------"
-
+./RELEASE_BUILD/tokki p $1 out.txt
 
 # As an example, I have provided how I would run my tokki.pyc.
 # python3 tokki.pyc $1 > out.txt # TODO: REPLACE THIS WITH YOUR OWN COMMAND.
